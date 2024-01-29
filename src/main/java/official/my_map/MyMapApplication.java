61e712e5ed18be3b466b8f22776377a957b78e17
Minseok Kim <minseokkim@Minseokui-MacBookPro-2.local>
2024-01-29 16:25:21 +0900

    [chore] 기본 설정

---
+++ src/main/java/official/my_map/MyMapApplication.java
@@ -0,0 +1,13 @@
+package official.my_map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/official/my_map/MyMapApplicationTests.java
@@ -0,0 +1,13 @@
+package official.my_map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
